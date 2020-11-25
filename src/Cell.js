17660ff0f97faa45b5680cd372a71488f8b7f1dd
Shamel Lakin <christlife4u@gmail.com>
2020-11-25 10:53:45 -0500

    Done.

---
+++ src/Cell.js
@@ -0,0 +1,18 @@
+class Cell extends React.Component {
+ 
+    // we use the constructor to set the INITIAL STATE
+    constructor() {
+      super()
+      this.state = {
+        value: color 
+      }
+    }
+
+    render() {
+        return (
+            <div className= "cell">
+
+            </div>
+        )
+    }
+}
